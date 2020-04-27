b2c156ce5b7444ce92cb2a3c8bd8168bb0f706f4
Michal Tvrznik <emporeor@gmail.com>
2020-04-27 20:36:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# ajsf
+__(a)nother (j)ava(s)cript (f)ramework__
+
+Tiny JavaScript Framework making web sites more vivid. Works with [nunjs](https://github.com/tvrzna/nunjs) since 0.0.3, or other jQuery-like library.
+
+---
+
+_Work in progress, stay tuned._
+
+---
+
+### API
+##### ajsf
+_since 0.0.1_
+
+TODO
+
+##### ajsf-bind
+_since 0.0.1_
+
+TODO
+
+##### ajsf-change
+_since 0.0.1_
+
+TODO
+
+##### ajsf-class
+_since 0.0.1_
+
+TODO
+
+##### ajsf-click
+_since 0.0.1_
+
+TODO
+
+##### ajsf-hide
+_since 0.0.1_
+
+TODO
+
+##### ajsf-keyup
+_since 0.0.1_
+
+TODO
+
+##### ajsf-repeat
+_since 0.0.1_
+
+TODO
+
+##### ajsf-show
+_since 0.0.1_
+
+TODO
+
+<div style="text-align: center;"><a href="https://xkcd.com/927/"><img src="https://imgs.xkcd.com/comics/standards.png" /></div>
---
+++ ajsf.js
@@ -0,0 +1,302 @@
+/**
+	ajsf 0.0.1
+
+	https://github.com/tvrzna/ajsf
+**/
+Ajsf = {
+	init: function(app, el) {
+		el.find('[ajsf-bind]').each(function(i, e) {
+			var $e = $(e);
+			var attr = $e.attr('ajsf-bind');
+
+			if (Ajsf.digObject(app, attr) === undefined) {
+				Ajsf.digObject(app, attr, '');
+			}
+			if ($e.prop('tagName') === 'INPUT') {
+				var event;
+				switch ($e.prop('type')) {
+					case "text":
+					case "password":
+					case "email":
+					case "url":
+					case "search":
+					case "number":
+						event = "keydown";
+						break;
+					case "submit":
+						break;
+					default:
+						event = "change";
+				}
+
+				if (event !== undefined) {
+					$e.on(event, function() {
+						setTimeout(function() {
+							Ajsf.digObject(app, attr, Ajsf.getVal(e));
+							Ajsf.refresh(app, el, attr);
+						}, 0);
+					});
+				}
+			}
+		});
+		el.find('[ajsf-click]').each(function(i, e) {
+			var $e = $(e);
+			var attr = $e.attr('ajsf-click');
+
+			$e.on('click', function(event) {
+				Ajsf.digObject(app, attr)(event, e);
+			});
+		});
+
+		el.find('[ajsf-keyup]').each(function(i, e) {
+			var $e = $(e);
+			var attr = $e.attr('ajsf-keyup');
+
+			$e.on('keyup', function(event) {
+				Ajsf.digObject(app, attr)(event, e);
+			});
+		});
+
+		el.find('[ajsf-change]').each(function(i, e) {
+			var $e = $(e);
+			var attr = $e.attr('ajsf-change');
+
+			$e.on('change', function(event) {
+				Ajsf.digObject(app, attr)(event, e);
+			});
+		});
+		Ajsf.refresh(app, el);
+	},
+	digObject: function(app, attr, val) {
+		var expression = attr.split('|');
+
+		var attribute = expression[0].trim();
+		var negative = false;
+		if (attribute.startsWith('!')) {
+			attribute = expression[0].substring(1).trim();
+			negative = true;
+		}
+
+		var arr = attribute.split('.'), i = 0, prop, obj = app.context;
+
+		for(; i < arr.length - 1; i++) {
+			if (arr[i] !== undefined && obj[arr[i]] !== undefined) {
+				prop = arr[i];
+				obj = obj[prop];
+			}
+		}
+
+		if (val !== undefined) {
+			obj[arr[i]] = val;
+			return;
+		}
+
+		var result;
+
+		if (negative) {
+			result = !obj[arr[i]];
+		} else {
+			result = obj[arr[i]];
+		}
+
+		for (var j = 1; j < expression.length; j++) {
+			if (expression[j] != undefined) {
+				result = Ajsf.filter(app, expression[j].trim(), result);
+			}
+		}
+
+		return result;
+	},
+	filter: function(app, expression, value) {
+		if (value === undefined || value === null) {
+			return value;
+		}
+
+		var method, param;
+
+		expression = expression.replace(/\s\s+/g, ' ');
+		method = expression.split(' ')[0];
+
+
+		if (expression.indexOf(' ') > 0) {
+			param = expression.substring(expression.indexOf(' ')).trim();
+			if (param !== undefined) {
+				if ((param.startsWith('\'') && param.endsWith('\'')) || (param.startsWith('"') && param.endsWith('"'))) {
+					param = param.substring(1, param.length - 1);
+				} else if (!isNaN(param)) {
+					param = Number(param);
+				} else {
+					param = Ajsf.digObject(app, param);
+				}
+			}
+		}
+
+		switch (method.toLowerCase()) {
+			case 'gt':
+				return value > param;
+			case 'gte':
+				return value >= param;
+			case 'lt':
+				return value < param;
+			case 'lte':
+				return value <= param;
+			case 'eq':
+				return value == param;
+			case 'ne':
+				return value != param;
+			case 'limit':
+				return value.slice(0, Number(param));
+			default:
+				console.warn('Filter ' + method + ' is not implemented yet.')
+				return value;
+		}
+	},
+	refresh: function(app, el, attr) {
+		var prefix = ':not([ajsf-repeated]) > ';
+		if (el.is('[ajsf-repeated]')) {
+			prefix = '';
+		}
+
+		var suffix = '';
+		if (attr !== undefined && Ajsf.digObject(app, attr) !== undefined) {
+			suffix = '="' + attr +'"';
+		}
+
+		el.find(prefix + '[ajsf-bind' + suffix +']').each(function(i, e) {
+			if (suffix === '') {
+				attr = $(e).attr('ajsf-bind');
+			}
+			Ajsf.setVal(e, Ajsf.digObject(app, attr));
+		});
+
+		el.find(prefix + '[ajsf-hide' + suffix + ']').each(function(i, e) {
+			var $e = $(e);
+
+			if (suffix === '') {
+				attr = $e.attr('ajsf-hide');
+			}
+			if (Ajsf.digObject(app, attr)) {
+				$e.hide();
+			} else {
+				$e.show();
+			}
+		});
+
+		el.find(prefix + '[ajsf-show' + suffix + ']').each(function(i, e) {
+			var $e = $(e);
+
+			if (suffix === '') {
+				attr = $e.attr('ajsf-show');
+			}
+			if (Ajsf.digObject(app, attr)) {
+				$e.show();
+			} else {
+				$e.hide();
+			}
+		});
+
+		el.find(prefix + '[ajsf-class]').each(function(i, e) {
+			var $e = $(e);
+			attr = JSON.parse(JSON.stringify(eval('(' + $e.attr('ajsf-class') + ')')));
+
+			for (var key in attr) {
+				var value = Ajsf.digObject(app, key);
+
+				if (value) {
+					if (!$e.hasClass(attr[key])) {
+						$e.addClass(attr[key]);
+					}
+				} else {
+					$e.removeClass(attr[key]);
+				}
+			}
+		});
+
+		el.find(prefix + '[ajsf-repeated' + suffix +']').each(function(i, e) {
+			$(e).remove();
+		});
+
+		el.find(prefix + '[ajsf-repeat' + suffix + ']').each(function(i, e) {
+			var $e = $(e);
+			$e.hide();
+			if (suffix === '') {
+				attr = $e.attr('ajsf-repeat');
+			}
+			var arr = Ajsf.digObject(app, attr);
+			if (arr !== undefined) {
+				for (var item in arr) {
+					var $clone = $e.clone().removeAttr('ajsf-repeat').attr('ajsf-repeated', 'true').show().insertBefore($e);
+
+					var subapp = {
+						context: {}
+					};
+					subapp.context.parent = JSON.parse(JSON.stringify(app.context));
+					subapp.context.item = arr[item];
+					subapp.context.item.index = item;
+					Ajsf.init(subapp, $clone);
+				}
+			}
+		});
+	},
+	getVal: function(e) {
+		var $e = $(e);
+		switch ($e.prop('type')) {
+			case "checkbox":
+				return $e.prop('checked');
+			case "radio":
+				var attr = $e.attr('ajsf-bind');
+				var arr = $('input[ajsf-bind=' + attr +'][type=radio]');
+				for (var i = 0; i <= arr.length; i++) {
+					$e = $(arr[i]);
+					if ($e.prop('checked')) {
+						return $e.val();
+					}
+				}
+				return null;
+			default:
+				return $e.val();
+		}
+	},
+	setVal: function(e, value) {
+		var $e = $(e);
+		if ($e.prop('tagName') === 'INPUT') {
+			switch ($e.prop('type')) {
+				case "checkbox":
+					$e.prop('checked', value);
+					break;
+				case "radio":
+					$e.prop('checked', e.value == value);
+					break;
+				default:
+					$e.val(value);
+			}
+		} else if ($e.prop('tagName') === 'SPAN' || $e.prop('tagName') === 'DIV' || $e.prop('tagName') === 'P' || $e.prop('tagName') === 'BUTTON') {
+			$e.text(value);
+		} else {
+			console.warn('Report element ' + $e.prop('tagName') + ' was missed, please.');
+		}
+	}
+};
+
+window.ajsf = function(selector, definition) {
+	var root = $('[ajsf="' + selector + '"]');
+
+	var instance = {};
+
+	instance.context = {
+		refresh: function() {
+			Ajsf.refresh(instance, root);
+		}
+	};
+
+	var def = definition(instance.context);
+	for (var key in def) {
+		instance.context[key] = def[key];
+	}
+
+	$(document).ready(function() {
+		Ajsf.init(instance, root);
+	});
+
+	return instance;
+};
